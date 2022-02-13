2809fec04b2583c1716639fe3749a00dd4d8e5b3
Evgeniy-Zh <othz@mail.ru>
2022-02-13 19:15:11 +0300

    Initial commit

---
+++ app/src/main/java/com/patron/patrontestlib/PatronUtils.kt
@@ -0,0 +1,10 @@
+package com.patron.patrontestlib
+
+import android.content.Context
+import com.google.android.flexbox.FlexboxLayout
+
+class PatronUtils {
+    fun evaluate(context: Context) {
+        FlexboxLayout(context)
+    }
+}
